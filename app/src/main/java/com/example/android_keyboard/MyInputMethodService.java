2accf983b3a3e3595c69eb345a2322b109b8c5a5
Haoyang <hygzhu@gmail.com>
2018-04-10 22:33:44 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android_keyboard/MyInputMethodService.java
@@ -0,0 +1,84 @@
+package com.example.android_keyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+/**
+ * Created by Haoyang on 4/10/2018.
+ */
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    @Override
+    public View onCreateInputView() {
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.key_values);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        keyboardView.setPreviewEnabled(false);
+        return keyboardView;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+        if(inputConnection !=  null){
+            switch(primaryCode){
+                case Keyboard.KEYCODE_DELETE:
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+                    if(TextUtils.isEmpty(selectedText)){
+                        inputConnection.deleteSurroundingText(1,0);
+                    }else{
+                        inputConnection.commitText("",1);
+                    }
+                    break;
+                case Keyboard.KEYCODE_DONE:
+                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                    break;
+                default:
+                    char code = (char) primaryCode;
+                    inputConnection.commitText(String.valueOf(code),1);
+            }
+        }
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
